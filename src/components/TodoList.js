e884c43845053e3aea93a75fbb4f59c298cd1cab
Sera <serano0123@gmail.com>
2019-03-01 05:28:14 +0900

    create TODO form

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,27 @@
 import React, { Component } from "react";
+
 export default class TodoList extends Component {
 
-  static defaultProps = {
-    todos: []
+  constructor(props) {
+    super(props);
+    this.state = {
+      todo: {
+        content: ''
+      },
+    };
   }
+
+  static defaultProps = {
+      todos: [],
+      onAdd: () => null
+  }
+
+  todoForm = () => (
+    <div>
+      <span><input type="text" placeholder="TODO" value={this.state.todo.content} onChange={this.handleChange.bind(this, 'content')} /></span>
+      <button onClick={this.handleOnAdd}>追加</button>
+    </div>
+  );
 
   renderTodo = (todo) => (
     <li key={todo.id}>
@@ -11,11 +29,32 @@ export default class TodoList extends Component {
     </li>
   );
 
+  handleChange = (field, { target: { value }}) => {
+    const { todo } = this.state;
+    todo[field] = value;
+    this.setState({ todo });
+  };
+
+  handleOnAdd = () => {
+    if (!this.state.todo.content) {
+      return;
+    }
+    const newTodo = {
+      content: this.state.todo.content
+    }
+    this.props.onAdd(newTodo);
+
+    const { todo } = this.state;
+    todo.content = '';
+    this.setState({ todo });
+  };
+
   render() {
-    const { todos } = this.props;
+    const { todos } = this.props; //graphql compose
     const Todos = todos ? todos.map(x => x.node) : []
     return (
       <div>
+        {this.todoForm()}
         <ul>
           {Todos.map(this.renderTodo)}
         </ul>
